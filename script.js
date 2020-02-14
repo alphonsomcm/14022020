c0eb4e334ccce0f34f21eeebf08be112aa08233b
alphonsomcm <alphonsomarcelo.mariano.iics@ust.edu.ph>
2020-02-14 14:28:58 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const success = "Passwords match."
+const error = "Passwords do not match"
+const comparePw = (para1, para2) => {
+    if ((document.getElementById(para1).value) == (document.getElementById(para2).value)) {
+        alert(success);
+    } else {
+        alert(error);
+    }
+}
